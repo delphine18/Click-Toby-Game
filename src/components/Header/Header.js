68cbc76b2e278e27f2814db3af77699616693441
Delphine <laurdel@gmail.com>
2018-12-24 22:41:43 -0600

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Toby Game!</h1>
+    <h2>
+      Click on an image of a dog to earn points, but don't click on any more than once!
+    </h2>
+  </header>
+);
+
+export default Header;
