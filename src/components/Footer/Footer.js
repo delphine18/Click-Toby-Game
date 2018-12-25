2c04ef367bb9fd8973b9d9a4fc38a56045c1ac60
Delphine <laurdel@gmail.com>
2018-12-25 13:28:29 -0600

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => (
   <footer className="footer">
     <div className="bottom">
-      Clicky Game! <img alt="react" src="assets/images/react.svg" />
+      Toby Game! <img alt="react" src="assets/images/react.svg" />
     </div>
   </footer>
 );
